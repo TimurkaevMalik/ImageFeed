0c5ea1c9a686ab5519d624f75369cdbbd0212222
Malik <timurkaev_malik@icloud.com>
2024-01-21 22:50:35 +0300

    sprint_08 has been finished

---
+++ ImageFeed/imageList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Malik Timurkaev on 17.01.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var cellImage: UIImageView!
+}
