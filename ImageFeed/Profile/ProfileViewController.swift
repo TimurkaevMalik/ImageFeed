a05806151fc9ae4ef1dc78bd42e131b2381613f4
Malik <timurkaev_malik@icloud.com>
2024-01-23 11:56:19 +0300

    Спринт 9/20: 9 → Тема 1/6: Онбординг → Урок 1/1

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,11 +2,20 @@
 //  ProfileViewController.swift
 //  ImageFeed
 //
-//  Created by Malik Timurkaev on 22.01.2024.
+//  Created by Malik Timurkaev on 23.01.2024.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
 }
