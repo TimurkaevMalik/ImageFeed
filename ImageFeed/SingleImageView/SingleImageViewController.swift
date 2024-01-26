b1d2533d170a954ef8c43a494df4d7fa55ad6bd1
Malik <timurkaev_malik@icloud.com>
2024-01-26 23:20:03 +0300

    Спринт 9/20: 9 → Тема 2/6: Навигация → Урок 4/7

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Malik Timurkaev on 25.01.2024.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
